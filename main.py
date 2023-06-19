e5345271919840b6426a3ca58a1702a31d3c0bc2
koudui <38391940+koudui@users.noreply.github.com>
2023-06-19 16:10:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def get_weather_text():
         info = '别忘了带雨伞哦~'
     elif weather_data.get('temp') <= 15:
         info = '天凉了，记得添件衣服~'
-    text = '%s天气%s，温度%s°C，%s。' % (city, weather_data.get('weather'), weather_data.get('temp'), info)
+    text = '%s天气%s，温度%s°C，%s' % (city, weather_data.get('weather'), weather_data.get('temp'), info)
     return text
 
 
